4471836e8c06623d85aad30e2004d6f3a1624e86
MoreTore <ryleymccarroll22@gmail.com>
2025-04-08 05:53:27 +0200

    change release target

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   manifests: {
-    release: 'https://raw.githubusercontent.com/commaai/openpilot/release3/system/hardware/tici/all-partitions.json',
+    release: 'https://storage.konik.ai/agnosupdate/11.13/all-partitions.json',
     master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/all-partitions.json',
   },
   loader: {
