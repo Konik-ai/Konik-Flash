be9090c2f9af7abfcf032404e4f25476750b4d90
ChosenCypher <hugs@duck.com>
2025-07-16 19:16:35 +0000

    remove openpilot versions

--- src/config.js
+++ src/config.js
@@ -2,19 +2,19 @@ const config = {
   versions: [
     {
       id: 'agnos-12.4',
-      name: 'AGNOS 12.4 (openpilot 0.9.9)',
+      name: 'AGNOS 12.4',
       manifest: 'https://storage.konik.ai/agnosupdate/12.4/all-partitions.json',
       isLatest: true,
     },
     {
       id: 'agnos-11.13',
-      name: 'AGNOS 11.13 (openpilot 0.9.8)',
+      name: 'AGNOS 11.13',
       manifest: 'https://storage.konik.ai/agnosupdate/11.13/all-partitions.json',
       isLatest: false,
     },
     {
       id: 'agnos-10.1',
-      name: 'AGNOS 10.1 (openpilot 0.9.7)',
+      name: 'AGNOS 10.1',
       manifest: 'https://storage.konik.ai/agnosupdate/10.1/all-partitions.json',
       isLatest: false,
     },
