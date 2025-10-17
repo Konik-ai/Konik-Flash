51094e53982675961e3a168a1fac9a356b8bc8af
ChosenCypher <chosencypher@duck.com>
2025-10-17 21:55:16 +0300

    add AGNOS 13.1 as latest

--- src/config.js
+++ src/config.js
@@ -1,10 +1,16 @@
 const config = {
   versions: [
     {
+      id: 'agnos-13.1',
+      name: 'AGNOS 13.1',
+      manifest: 'https://storage.konik.ai/agnosupdate/13.1/all-partitions.json',
+      isLatest: true,
+    },
+    {
       id: 'agnos-12.4',
       name: 'AGNOS 12.4',
       manifest: 'https://storage.konik.ai/agnosupdate/12.4/all-partitions.json',
-      isLatest: true,
+      isLatest: false,
     },
     {
       id: 'agnos-11.13',
