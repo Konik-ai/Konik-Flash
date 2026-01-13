ae09c1aa5aab134981d095c30351bc4f8e914051
Amogh Munikote <amogh.munikote@gmail.com>
2026-01-12 22:03:20 -0800

    AGNOS 15.1

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const isDev = import.meta.env.DEV
-const manifestBaseUrl = isDev ? '/agnosupdate' : 'https://storage.konik.ai/agnosupdate'
+const manifestBaseUrl = isDev ? '/agnosupdate' : 'https://raw.githubusercontent.com/hoofpilot/hoofpilot'
 const loaderUrl = isDev
   ? '/qdl/programmer.bin'
   : 'https://raw.githubusercontent.com/commaai/flash/master/src/QDL/programmer.bin'
@@ -7,22 +7,10 @@ const loaderUrl = isDev
 const config = {
   versions: [
     {
-      id: 'agnos-13.1',
-      name: 'AGNOS 13.1',
-      manifest: `${manifestBaseUrl}/13.1/all-partitions.json`,
+      id: 'agnos-15.1',
+      name: 'AGNOS 15.1',
+      manifest: `${manifestBaseUrl}/master/system/hardware/tici/all-partitions.json`,
       isLatest: true,
-    },
-    {
-      id: 'agnos-12.4',
-      name: 'AGNOS 12.4',
-      manifest: `${manifestBaseUrl}/12.4/all-partitions.json`,
-      isLatest: false,
-    },
-    {
-      id: 'agnos-11.13',
-      name: 'AGNOS 11.13',
-      manifest: `${manifestBaseUrl}/11.13/all-partitions.json`,
-      isLatest: false,
     },
   ],
   loader: {
